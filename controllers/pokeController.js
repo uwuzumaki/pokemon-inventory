f2efd78e3e63456b6e03f1ab352ad4449d39e54e
Kris Wong <krischwong@gmail.com>
2025-01-05 22:57:32 -0500

    Added all basic routes, controllers, and ejs

--- controllers/pokeController.js
+++ controllers/pokeController.js
@@ -2,6 +2,26 @@ const homepage = (req, res) => {
   res.render("homepage", { title: "Pokemon Search" });
 };
 
+const pokemon = async (req, res) => {
+  res.render("pokemon", { title: "Pokemon Search" });
+};
+
+const pokeRegion = async (req, res) => {
+  res.render("pokeRegion", { title: "Pokemon Search" });
+};
+
+const pokeType = async (req, res) => {
+  res.render("poketype", { title: "Pokemon Search" });
+};
+
+const pokeTrainer = async (req, res) => {
+  res.render("poketrainer", { title: "Pokemon Search" });
+};
+
 module.exports = {
   homepage,
+  pokemon,
+  pokeRegion,
+  pokeType,
+  pokeTrainer,
 };
--- routes/pokeRouter.js
+++ routes/pokeRouter.js
@@ -3,5 +3,9 @@ const pokeController = require("../controllers/pokeController");
 const pokeRouter = Router();
 
 pokeRouter.get("/", pokeController.homepage);
+pokeRouter.post("/", pokeController.pokemon);
+pokeRouter.get("/region", pokeController.pokeRegion);
+pokeRouter.get("/type", pokeController.pokeType);
+pokeRouter.get("/trainer", pokeController.pokeTrainer);
 
 module.exports = pokeRouter;
