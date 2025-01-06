61f3ba610df984d0a376b5c8b6cf7afbeacb57ff
Kris Wong <krischwong@gmail.com>
2025-01-05 21:21:07 -0500

    Added tailwind to project

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const pokeRouter = require("./routes/pokeRouter");
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", pokeRouter);
 
