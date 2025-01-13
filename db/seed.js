b37d51ad2483e78c28b800e6710ee7f406b1d0d5
Kris Wong <krischwong@gmail.com>
2025-01-12 20:47:56 -0500

    Added batch querying to api endpoint

--- db/seed.js
+++ db/seed.js
@@ -3,10 +3,10 @@ const { Client } = require("pg");
 
 // Gets pokemon from PokemonAPI using pagination
 const pokemon = async (limit, offset) => {
-  const url = "https://pokeapi.co/api/v2/pokemon/";
+  const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
   try {
     // Gets a list of the pokemon name and their url
-    const response = await fetch(url, { limit: limit, offset: offset });
+    const response = await fetch(url);
     const pokemonUrl = await response.json();
 
     // Transforms the data so that it's table ready
@@ -36,10 +36,23 @@ const types = async () => {
   }
 };
 
+const fetchAndStore = async () => {
+  const batchSize = 100;
+  const total = 1302;
+
+  for (let offset = 0; offset < 200; offset += batchSize) {
+    console.log(`Fetching Pokemon ${offset + 1} to ${offset + batchSize}`);
+    const pokeBatch = await pokemon(batchSize, offset);
+    console.log(pokeBatch);
+    console.log("Inserting...");
+  }
+};
+
 const main = async () => {
-  const wowee = await pokemon(10, 10);
-  const type = await types();
-  console.log(type);
+  const wowee = await fetchAndStore();
+  // const type = await types();
+  // console.log(wowee);
+  // console.log(type);
 };
 
 main();
