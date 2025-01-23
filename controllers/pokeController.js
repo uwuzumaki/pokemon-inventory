0f53396a5d88176dc5e822c79c84d8e66cbf7274
Kris Wong <krischwong@gmail.com>
2025-01-22 23:56:12 -0500

    Added sorting by region

--- controllers/pokeController.js
+++ controllers/pokeController.js
@@ -27,8 +27,52 @@ const pokeRegion = async (req, res) => {
 };
 
 const pokeRegionPost = async (req, res) => {
-  console.log("123");
-  res.render("123");
+  const gens = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  let pokemon;
+  switch (req.params.gen) {
+    case "1":
+      pokemon = await db.getOneGen(1, 151);
+      break;
+    case "2":
+      pokemon = await db.getOneGen(152, 251);
+      break;
+    case "3":
+      pokemon = await db.getOneGen(252, 386);
+      break;
+    case "4":
+      pokemon = await db.getOneGen(387, 493);
+      break;
+    case "5":
+      pokemon = await db.getOneGen(494, 649);
+      break;
+    case "6":
+      pokemon = await db.getOneGen(650, 721);
+      break;
+    case "7":
+      pokemon = await db.getOneGen(722, 809);
+      break;
+    case "8":
+      pokemon = await db.getOneGen(810, 905);
+      break;
+    case "9":
+      pokemon = await db.getOneGen(906, 1025);
+      break;
+  }
+  res.render("pokeRegionDex", {
+    title: "Pokemon Search",
+    pokemon: pokemon,
+    gens: gens,
+  });
 };
 
 const pokeType = async (req, res) => {
--- db/queries.js
+++ db/queries.js
@@ -26,6 +26,33 @@ WHERE
   return rows;
 };
 
+const getOneGen = async (start, end) => {
+  const { rows } = await pool.query(
+    `SELECT
+   p.id AS pokemon_id,
+   p.NAME AS pokemon_name,
+   t1.type_name AS type1_name,
+   t2.type_name AS type2_name 
+FROM
+   pokemon_type pt 
+   JOIN
+      pokemon p 
+      ON pt.pokemon_id = p.id 
+   JOIN
+      TYPE t1 
+      ON pt.type_id1 = t1.id 
+   LEFT JOIN
+      TYPE t2 
+      ON pt.type_id2 = t2.id 
+WHERE
+   p.id >= $1 AND p.id <= $2
+ `,
+    [start, end]
+  );
+  return rows;
+};
+
 module.exports = {
   getOnePokemon,
+  getOneGen,
 };
