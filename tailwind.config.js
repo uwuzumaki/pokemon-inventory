ff33d917ccbe3125f3eceb74d1020375a6ee020f
Kris Wong <krischwong@gmail.com>
2025-03-18 23:00:17 -0400

    Changed so it also processes the partials folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.ejs"],
+  content: ["./views/**/*.ejs"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: ["@tailwindcss/typography"],
 };
